2e387a0e0cfd2ef03c77631d57baa33c62e9127f
Adarsh keshari <64305181+Adarsh-044@users.noreply.github.com>
2023-08-06 11:55:43 +0530

    express-promise-router added

--- Express/11_Routes_module.js
+++ Express/11_Routes_module.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const { purchaseFruit, updateFruit } = require('./12_controllers');
 
-
-
 const fruitRouter = express.Router();
 
 fruitRouter.route("/")
@@ -23,3 +21,17 @@ fruitRouter.route
 
 // export the entire router so we can require() it in server.js
 module.exports = fruitRouter;   // here module.exports.fruitRouter is not because it is only providing the path not the object
+
+
+
+
+
+// Method - 2: It basically handles promises automatically, while the above Router() can't handle it. 
+const router = require('express-promise-router');
+const fruitRoute = router();
+fruitRoute.route('/')
+      .get((req, res) => {
+            res.send('Fruit Route');
+      })
+module.exports = fruitRoute; 
+
